5429855039197400fc9126f36c6be5f42c14b5dc
Saikiran <saikiran5494@gmail.com>
2023-04-15 06:08:58 +0000

    finalCode

---
+++ src/components/BrowserHistoryItem/index.js
@@ -0,0 +1,39 @@
+import './index.css'
+
+const BrowserHistoryItem = props => {
+  const {initialHistoryList, iconClicked} = props
+  const {timeAccessed, logoUrl, title, domainUrl, id} = initialHistoryList
+
+  const onDelete = () => {
+    iconClicked(id)
+  }
+
+  return (
+    <li className="history-item">
+      <div className="browser-start">
+        <p className="history-time">{timeAccessed}</p>
+        <div className="browser-middle">
+          <img src={logoUrl} className="history-image" alt="domain logo" />
+          <p className="history-title">{title}</p>
+          <p className="history-url">{domainUrl}</p>
+        </div>
+      </div>
+      <div className="browser-end">
+        <button
+          className="delete-button"
+          type="button"
+          onClick={onDelete}
+          data-testid="delete"
+        >
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
+            className="delete-image"
+            alt="delete"
+          />
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default BrowserHistoryItem
